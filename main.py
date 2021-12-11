fb5efbbdcc4f9ccead53c1dc335f2972c94d98ea
shailvestein <62176093+shailvestein@users.noreply.github.com>
2021-12-11 23:43:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -95,11 +95,11 @@ with st.form("input_form"):
     # source: https://discuss.streamlit.io/t/change-font-size-and-font-color/12377/3
     enter_review_here = '<p style="color:Black; font-size: 20px;">Your review goes here</p>'
     st.markdown(enter_review_here, unsafe_allow_html=True)
-    review = st.text_input(label='')
+    review = st.text_input(label='', value='review')
     st.markdown('')
     st.markdown('')
     # Predict polarity button
-    submitted = st.form_submit_button("Predict Polarity")
+    submitted = st.form_submit_button("Analyse review")
 
 # preprocessing input review
 review = preprocess(review)
